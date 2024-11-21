e5a6667b427b9cf98bc51616265c8c838d3df7e9
Valentin Pratz <git@valentinpratz.de>
2024-11-21 07:49:10 +0000

    utils: change autograd backend for torch jvp
    Change from `torch.autograd.functional.jvp` to
    `torch.func.jvp`, as recommended in the documentation.
    https://pytorch.org/docs/stable/generated/torch.autograd.functional.jvp.html
    
    Using autograd.functional seems to break the gradient flow, while `func`
    does not produce problems in this regard.

--- bayesflow/utils/jvp.py
+++ bayesflow/utils/jvp.py
@@ -8,24 +8,20 @@ def jvp(fn: callable, primals: tuple[Tensor] | Tensor, tangents: tuple[Tensor] |
     the input (primals) and vectors in tangents."""
 
     match keras.backend.backend():
+        case "jax":
+            import jax
+
+            fn_output, _jvp = jax.jvp(fn, primals, tangents)
         case "torch":
             import torch
 
-            fn_output, _jvp = torch.autograd.functional.jvp(fn, primals, tangents)
+            fn_output, _jvp = torch.func.jvp(fn, primals, tangents)
         case "tensorflow":
             import tensorflow as tf
 
             with tf.autodiff.ForwardAccumulator(primals=primals, tangents=tangents) as acc:
                 fn_output = fn(*primals)
             _jvp = acc.jvp(fn_output)
-        case "jax":
-            import jax
-
-            fn_output, _jvp = jax.jvp(
-                fn,
-                primals,
-                tangents,
-            )
         case _:
             raise NotImplementedError(f"JVP not implemented for backend {keras.backend.backend()}")
     return fn_output, _jvp
