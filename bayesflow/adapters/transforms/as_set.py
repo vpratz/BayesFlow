9deda9d391cc0727d908749d60adbbea5173f598
Leona Odole <eldaleona-odole@uiowa.edu>
2024-11-30 12:10:21 +0100

    explaination of purpose of ad_set with useage

--- bayesflow/adapters/transforms/as_set.py
+++ bayesflow/adapters/transforms/as_set.py
@@ -4,6 +4,12 @@ from .elementwise_transform import ElementwiseTransform
 
 
 class AsSet(ElementwiseTransform):
+    """
+    The `.as_set(["x", "y"])` transform indicates that both `x` and `y` are treated as sets. 
+    That is, their values will be treated as *exchangable* such that they will imply the same inference regardless of the values' order. 
+    This would be useful in a linear regression context where we can index the observations in arbitrary order and always get the same regression line.
+    """
+
     def forward(self, data: np.ndarray, **kwargs) -> np.ndarray:
         return np.atleast_3d(data)
 
