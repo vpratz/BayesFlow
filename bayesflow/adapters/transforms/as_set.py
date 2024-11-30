d29ff5a96878aa2e1d1ea03466f992388fc121b3
Leona Odole <eldaleona-odole@uiowa.edu>
2024-11-30 12:38:42 +0100

    added documentation of constrain transform

--- bayesflow/adapters/transforms/as_set.py
+++ bayesflow/adapters/transforms/as_set.py
@@ -8,6 +8,13 @@ class AsSet(ElementwiseTransform):
     The `.as_set(["x", "y"])` transform indicates that both `x` and `y` are treated as sets. 
     That is, their values will be treated as *exchangable* such that they will imply the same inference regardless of the values' order. 
     This would be useful in a linear regression context where we can index the observations in arbitrary order and always get the same regression line.
+
+    Useage: 
+    
+    adapter = (
+        bf.Adapter()
+        .as_set(["x", "y"])
+        )
     """
 
     def forward(self, data: np.ndarray, **kwargs) -> np.ndarray:
