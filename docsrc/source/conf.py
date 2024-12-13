2dd91da09e9136c1b1a81b34e667db0ed8810b71
Valentin Pratz <git@valentinpratz.de>
2024-12-13 16:55:10 +0000

    doc wip: add autosummary_imported_members to show imported classes/functions

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -72,6 +72,7 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+autosummary_imported_members = True
 
 
 # -- Options for HTML output -------------------------------------------------
