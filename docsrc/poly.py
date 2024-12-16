a04654739aa5eea830dda5f22e2c7803c3bd57dd
Valentin Pratz <git@valentinpratz.de>
2024-12-16 08:52:12 +0000

    docs: polyversion, WIP

---
+++ docsrc/poly.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from pathlib import Path
+
+from sphinx_polyversion.api import apply_overrides
+from sphinx_polyversion.driver import DefaultDriver
+from sphinx_polyversion.git import Git, GitRef, GitRefType, file_predicate, refs_by_type
+from sphinx_polyversion.pyvenv import Pip
+from sphinx_polyversion.sphinx import SphinxBuilder
+
+#: Regex matching the branches to build docs for
+BRANCH_REGEX = r"master-doctest"
+
+#: Regex matching the tags to build docs for
+TAG_REGEX = r"v1.1.6"
+
+#: Output dir relative to project root
+OUTPUT_DIR = "_polybuild"
+
+#: Source directory
+SOURCE_DIR = "docsrc/"
+
+#: Arguments to pass to `poetry install`
+POETRY_ARGS = "numpy"
+
+#: Arguments to pass to `sphinx-build`
+SPHINX_ARGS = "-a -v"
+
+#: Mock data used for building local version
+MOCK_DATA = {
+    "revisions": [
+        GitRef("v1.1.6", "", "", GitRefType.TAG, datetime.fromtimestamp(0)),
+        GitRef("master-doctest", "", "", GitRefType.BRANCH, datetime.fromtimestamp(3)),
+    ],
+    "current": GitRef("master-doctest", "", "", GitRefType.TAG, datetime.fromtimestamp(6)),
+}
+MOCK = True
+
+
+#: Data passed to templates
+def data(driver, rev, env):
+    revisions = driver.targets
+    branches, tags = refs_by_type(revisions)
+    latest = max(tags or branches)
+    return {
+        "current": rev,
+        "tags": tags,
+        "branches": branches,
+        "revisions": revisions,
+        "latest": latest,
+    }
+
+
+def root_data(driver):
+    revisions = driver.builds
+    branches, tags = refs_by_type(revisions)
+    latest = max(tags or branches)
+    return {"revisions": revisions, "latest": latest}
+
+
+# Load overrides read from commandline to global scope
+apply_overrides(globals())
+# Determine repository root directory
+root = Git.root(Path(__file__).parent)
+print("root", root)
+
+# Setup driver and run it
+src = Path(SOURCE_DIR)
+DefaultDriver(
+    root,
+    OUTPUT_DIR,
+    vcs=Git(
+        branch_regex=BRANCH_REGEX,
+        tag_regex=TAG_REGEX,
+        buffer_size=1 * 10**9,  # 1 GB
+        predicate=file_predicate([src]),  # exclude refs without source dir
+    ),
+    builder=SphinxBuilder(src / "source", args=SPHINX_ARGS.split()),
+    env=Pip.factory(args=POETRY_ARGS.split(), venv="buildvenv"),
+    template_dir=root / src / "polyversion/templates",
+    static_dir=root / src / "polyversion/static",
+    data_factory=data,
+    root_data_factory=root_data,
+    mock=MOCK_DATA,
+).run(MOCK)
--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -11,6 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
+import shutil
+from sphinx_polyversion import load
+from sphinx_polyversion.git import GitRef
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -87,7 +90,8 @@ html_title = "BayesFlow: Amortized Bayesian Inference"
 # Add any paths that contain custom _static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin _static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["css/custom.css"]
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 html_show_sourcelink = False
 html_theme_options = {
     "repository_url": "https://github.com/bayesflow-org/bayesflow",
@@ -102,6 +106,15 @@ html_logo = "_static/bayesflow_hex.png"
 html_favicon = "_static/bayesflow_hex.ico"
 html_baseurl = "https://www.bayesflow.org/"
 html_js_files = ["https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"]
+html_sidebars = {
+    "**": [
+        "navbar-logo.html",
+        "icon-links.html",
+        "search-button-field.html",
+        "sbt-sidebar-nav.html",
+        "versioning.html",
+    ],
+}
 
 todo_include_todos = True
 
@@ -120,3 +133,52 @@ suppress_warnings = [
 remove_from_toctrees = ["_autosummary/*"]
 
 autosummmary_generate = True
+
+# sphinx-multiversion: select tags, branches and remotes
+smv_tag_whitelist = r"^(v1.1.6)$"
+# smv_branch_whitelist = r"^(|doc-autosummary|master|dev)$"
+smv_branch_whitelist = r"^(master)$"
+smv_remote_whitelist = None
+
+
+# move files around if necessary
+def copy_files_handler(app, config):
+    print("TODO")
+    return
+    current_version = config["smv_current_version"]
+    current_metadata = config["smv_metadata"][current_version]
+    basedir = current_metadata["basedir"]
+    sourcedir = current_metadata["sourcedir"]
+
+    print("Current version:", current_version)
+    print("Basedir:", basedir)
+    print("Metadata:", current_metadata)
+
+    examples_src = os.path.join(basedir, "examples")
+    examples_dst = os.path.join(sourcedir, "_examples")
+    if os.path.exists(examples_src):
+        shutil.copytree(examples_src, examples_dst, dirs_exist_ok=True)
+    examples_in_progress = os.path.join(examples_dst, "in_progress")
+    if os.path.exists(examples_in_progress):
+        shutil.rmtree(examples_in_progress)
+    contributing_src = os.path.join(basedir, "CONTRIBUTING.md")
+    contributing_dst = os.path.join(sourcedir, "contributing.md")
+    if os.path.exists(contributing_src):
+        shutil.copy2(contributing_src, contributing_dst)
+    installation_src = os.path.join(basedir, "INSTALL.rst")
+    installation_dst = os.path.join(sourcedir, "installation.rst")
+    if os.path.exists(installation_src):
+        shutil.copy2(installation_src, installation_dst)
+
+
+def cleanup_handler(app, exception):
+    print("Done, what now?")
+
+
+def setup(app):
+    app.connect("config-inited", copy_files_handler)
+    app.connect("build-finished", cleanup_handler)
+
+
+data = load(globals())
+current: GitRef = data["current"]
